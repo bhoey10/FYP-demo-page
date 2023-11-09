2ce9740296b514c066a5ce38d54f8b67cbbb9aa5
bhoey10 <150317919+bhoey10@users.noreply.github.com>
2023-11-09 07:33:56 +0000

    Update app.module.ts with comments

--- app.module.ts
+++ app.module.ts
@@ -1,3 +1,5 @@
+//Imports that are required modules and components from Angular core and forms packages taken from Angular.
+//DemoPageComponent and reactive forms module also included
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -5,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DemoPageComponent } from './demo-page/demo-page.component';
 
-
+// A decorator to define the root components of the application
+// Declaring the main component of the application (AppComponent) and the DemoPageComponent.
 @NgModule({
   declarations: [
     AppComponent,
     DemoPageComponent
   ],
+  //Importing AppRoutingModule for routing and ReactiveFormsModule for having a reactive form
   imports: [
     BrowserModule,
     AppRoutingModule,
