0c92c84c3c0bae0e146be5df90f98dd84e7bf121
bhoey10 <150317919+bhoey10@users.noreply.github.com>
2023-11-09 07:32:43 +0000

    Update app-routing.module.ts with comments

--- app-routing.module.ts
+++ app-routing.module.ts
@@ -1,7 +1,11 @@
+//Imports that are required modules and components from Angular core and forms packages taken from Angular.
+//Importing the DemoPageComponent also
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DemoPageComponent } from './demo-page/demo-page.component';
 
+//The application routes using router module
+//The path to the DeomPageComponent is defined
 const routes: Routes = [
   {path: 'demo-page', component: DemoPageComponent},
 ];
